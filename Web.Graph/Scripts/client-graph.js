dcdf18a74a34404df77d6c6b3e8233b68f6f4b62
giansalex <giansalex@gmail.com>
2016-12-29 17:11:56 -0500

    Add php Example + Editor

--- Web.Graph/Scripts/client-graph.js
+++ Web.Graph/Scripts/client-graph.js
@@ -21,7 +21,7 @@ var syntaxHighlight = function (json) {
 }
 
 var setOut = function (jObject) {
-    $('#log').html(syntaxHighlight(JSON.stringify(jObject, undefined, 2)));
+    window.ace.edit("editorO").setValue(JSON.stringify(jObject, undefined, 2));
 
     console.log($('#Result').is(':visible'));
     $('html, body').animate({
@@ -32,11 +32,11 @@ var setOut = function (jObject) {
 
 var send = function () {
     var $frm = $(this);
-    $('#log').text("Ejecutando...");
+    window.ace.edit("editorO").setValue("Ejecutando...");
     $.ajax({
         url : $frm.attr('action'),
         method: $frm.attr('method'),
-        data: "=" + this.query.value,
+        data: "=" + window.ace.edit("editorQ").getValue(),
         dataType : 'json',
         error: function () {
             alert("Invalid Request");
@@ -47,3 +47,42 @@ var send = function () {
     return false;
 };
 $("#frmQuery").submit(send);
+var editor = ace.edit("editorQ");
+editor.getSession().setUseWorker(false);
+editor.getSession().setMode("ace/mode/json");
+editor.setTheme("ace/theme/xcode");
+editor.renderer.setShowGutter(false);
+editor.setShowPrintMargin(false);
+editor.setOptions({
+    enableBasicAutocompletion: true,
+    enableSnippets: false,
+    enableLiveAutocompletion: true
+});
+var staticWordCompleter = {
+    getCompletions: function (editor, session, pos, prefix, callback) {
+        var wordList = ["query", "empresa", "ruc", "nombre", "tipo_contribuyente", "profesion", "nombre_comercial", "condicion_contribuyente",
+            "estado_contribuyente", "fecha_inscripcion", "fecha_inicio", "departamento", "provincia", "distrito", "direccion",
+            "telefono", "fax", "comercio_exterior", "principal", "secundario2", "rus", "buen_contribuyente", "retencion",
+            "percepcion_vinterna", "percepcion_cliquido"];
+        callback(null, wordList.map(function (word) {
+            return {
+                caption: word,
+                value: word,
+                meta: "static"
+            };
+        }));
+    }
+}
+editor.completers = [staticWordCompleter];
+
+var editor2 = ace.edit("editorO");
+editor2.getSession().setUseWorker(false);
+editor2.getSession().setMode("ace/mode/json");
+editor2.setTheme("ace/theme/monokai");
+editor2.setReadOnly(true);
+editor2.renderer.setShowGutter(false);
+editor2.setShowPrintMargin(false);
+//Style editors
+var h = $(window).height() * 3/5;
+$('#editorQ').attr("style", "height: " + h + "px");
+$('#editorO').attr("style", "height: " + h + "px");//40rem
